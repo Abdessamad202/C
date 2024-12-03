f143d2897d967491dd01b2f71b578894f3fc4662
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-03 14:39:09 +0100

    Programe of 2 Integers

---
+++ SumTwoNumbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+  int num1,num2;
+  printf("enter the first number :");
+  scanf("%d",&num1);
+  printf("enter the second number :");
+  scanf("%d",&num2);
+  printf("the sum is :%d",num1+num2);
+  return 0 ;
+}
